39a8ef25431367ef0df1efd10dc94ac5d8e1787d
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2025-02-24 18:40:04 +0200

    #Feature: Adicionei o Create Handlebar

--- routes/admin.js
+++ routes/admin.js
@@ -19,12 +19,33 @@ router.get('/categorias/create', (req, res) => {
     res.render('admin/categorias/create');
 });
 router.post('/categorias/save', (req, res) => {
+    var errors = [];
+
+    if (!req.body.nome) {
+        errors.push({ text: 'Nome da categoria não pode ser vazio' });
+    }
+    if (!req.body.slug) {
+        errors.push({ text: 'Slug da categoria não pode ser vazio' });
+    }
+    if (req.body.nome.length < 2) {
+        errors.push({ text: 'Nome da categoria deve ter mais de 2 caracteres' });
+    }
+
+    if (errors.length > 0) {
+        return res.render('admin/categorias/create', { errors: errors }); 
+    }
+
     const newCategoria = new Categoria(req.body);
-    newCategoria.save().then(() => {
-        res.redirect('/admin/categorias');
-    }).catch((err) => {
-        res.send('Erro ao salvar categoria: ' + err);
-    });
+    newCategoria.save()
+        .then(() => {
+            req.flash('success_msg', 'Categoria criada com sucesso ✅');
+            return res.redirect('/admin/categorias'); 
+        })
+        .catch((err) => {
+            req.flash('error_msg', 'Erro ao salvar categoria: ' + err);
+            return res.send('Erro ao salvar categoria: ' + err); 
+        });
 });
+
 
 module.exports = router;
